7b0de8b29b3a4e5e59d66cce718e3efaa5d8924e
Zoe Haskell-Craig <zoe.haskellcraig@gmail.com>
2026-04-07 11:08:28 -0400

    updated to use years from 2014 to end of 2023

--- 002_temperaturedata.R
+++ 002_temperaturedata.R
@@ -1,17 +1,32 @@
 #######################################
 ## Load and clean temperature data
 ## Date Created: Nov 25th 2024
-## Last Modified: Nov 25th 2024
+## Last Modified: Feb 9 2026
 #######################################
 ## Using UTCI data from TEE: temperature extremes europe dataset
-
 
 # load data [Update path if necessary to match local environment] --------
 print("loading daily UTCI data...")
 
 # load yearly temperature data at NUTS 3 -----------
-utciyearly_nuts3 <- read_csv("Data/utciyearly_nuts3.csv")
+#utciyearly_nuts3 <- read_csv("Data/utciyearly_nuts3.csv")
+tempyearly_nuts3 <- read_csv("Data/teeyearly_nuts3.csv")
 
 # filter to years between 2014 and 2021 ------------
-utci_temperature_data <- utciyearly_nuts3 %>%
-  filter(year >= 2014 & year <= 2021)
+utci_temperature_data <- tempyearly_nuts3 %>%
+  select(nuts_id, year, utci_above_26) %>%
+  filter(year >= 2014 & year <= 2023)
+
+
+# Sensitivity Analysis: other measures of temperature extremes --------------------
+
+tempyearly_nuts3 <- read_csv("Data/teeyearly_nuts3.csv")
+
+## filter to years between 2014 and 2021 ------------
+tempyearly_nuts3 <- tempyearly_nuts3 %>%
+  filter(year >= 2014 & year <= 2023)
+
+
+
+
+
